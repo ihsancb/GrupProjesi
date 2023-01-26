62afa88b1d836f502141def914e07e5ba31ac0e6
ihsancb <ihsancanbozoklar@hotmail.com>
2023-01-26 12:22:38 +0300

    sildim

--- src/main/java/com/ba/Main.java
+++ src/main/java/com/ba/Main.java
@@ -3,11 +3,7 @@ package com.ba;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("qqqqqqqqqqqqqqqqq");
+        System.out.println("sildim");
 
-        System.out.println("ihsancb");
-
-        System.out.println("ihsancb");
     }
 }
